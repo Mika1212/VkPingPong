fc9911edc14b2e90b875fea4045b7682d75f91a1
Ilyas <ilravgaz03@mail.ru>
2024-08-18 17:29:19 +0300

    Deleted and renewed secret config data

--- src/main/java/ru/mika/vkpingpong/config/SecretConfig.java
+++ src/main/java/ru/mika/vkpingpong/config/SecretConfig.java
@@ -1,10 +1,10 @@
 package ru.mika.vkpingpong.config;
 
 public class SecretConfig {
-    private static final String acceptString = "99077c5b";
-    private static final String secretKey = "secretKeyVkBotJustAISecretVerySecret123";
-    private static final int groupId = 226973454;
-    private static final String accessToken = "vk1.a.oNUTB2lOsg9STxRXesuKl9SSdaG0SWYL1qUS-UP8T2eMWH4b39PwU-R2n8_oW07sKMbgmBtjtdaxqmZfD2xl7RTNAgDdoN9XqHiK5qE96sHk1mXsAsmdIj2vn3ogFDKZJnKvV8eky4Vv4p6k54mWCBqPYZ6DSSUbmdJKVNBPiO1gXMX6pQVh6iYaMQ606MT7BeHePjaF48UisEFevweQAw";
+    private static final String acceptString = "";
+    private static final String secretKey = "";
+    private static final int groupId = ;
+    private static final String accessToken = "";
 
     public static String getAcceptString() {
         return acceptString;
